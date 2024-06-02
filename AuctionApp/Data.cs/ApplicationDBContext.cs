e8ea92078d64b6632969a8ebea8b9cb16b5df5cd
rightone0130 <rightone0130@gmail.com>
2024-06-02 13:44:09 +0100

    initializing

---
+++ AuctionApp/Data.cs/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+using AuctionApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
